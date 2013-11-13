908ec7e4c6072c391a7c601a73a897826999189c
wudalong <wudalong@gmail.com>
2013-11-13 10:35:52 +0800

    初始化Maven项目

---
+++ src/main/java/com/taodian/emop/App.java
@@ -0,0 +1,13 @@
+package com.taodian.emop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
