b8c558e61203f919337f1769be96de061677328f
wudalong <wudalong@gmail.com>
2013-12-04 12:09:15 +0800

    修改状态显示格式

--- src/main/java/com/taodian/click/monitor/TextStatusDumper.java
+++ src/main/java/com/taodian/click/monitor/TextStatusDumper.java
@@ -138,7 +138,7 @@ public class TextStatusDumper {
 		//if(item.obj == null || !(item.obj instanceof ShopAccount)) return;
 		//ShopAccount s = (ShopAccount)item.obj;
 				
-		String msg = String.format("%s shop id:%s, balance:%3$1.2f, status:%4s", sdf.format(new Date(item.created)),
+		String msg = String.format("%1$s shop id:%2$s, balance:%3$1.2f, status:%4$s", sdf.format(new Date(item.created)),
 				item.shopId, item.banlance, item.status);
 		writer.println(msg);
 	}	
