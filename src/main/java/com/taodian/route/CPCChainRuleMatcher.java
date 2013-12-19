652d592a84146ea4175d7802b7805a457edb1e44
wudalong <wudalong@gmail.com>
2013-12-19 23:31:36 +0800

    更新RuleMatcher的文档。

---
+++ src/main/java/com/taodian/route/CPCChainRuleMatcher.java
@@ -0,0 +1,12 @@
+package com.taodian.route;
+
+public class CPCChainRuleMatcher implements RuleMatcher {
+
+	@Override
+	public boolean isMatch(Rule r, Rule r2, boolean isStrict) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+}
--- src/main/java/com/taodian/route/RuleMatcher.java
+++ src/main/java/com/taodian/route/RuleMatcher.java
@@ -9,5 +9,23 @@ package com.taodian.route;
  *
  */
 public interface RuleMatcher {
+	
+	/**
+	 * 检查两个规则是否匹配。把路由规则，和查询条件都转换为相同的形式进行比较。支持严格匹配，和模糊匹配
+	 * 两种方式， 严格匹配只两个规则的所有字段都需要一样。
+	 * 
+	 * 模糊匹配，只检查不为空的关键字段。
+	 * 例如：
+	 * r1.user_id = 74, uid = 0, short_key = '001'
+	 * r2.user_id = 74, uid = 1234, short_key = '001'
+	 * 
+	 * 在模糊匹配的情况下，两个规则匹配成功。因为r1 的uid 为空，表示不检查。在严格匹配的方式下，失败。
+	 * 因为uid 不一样。
+	 *  
+	 * @param r
+	 * @param r2
+	 * @param isStrict 是否进行严格的匹配检查。
+	 * @return true 如果两个规则匹配。
+	 */
 	public boolean isMatch(Rule r, Rule r2, boolean isStrict);
 }
